509490124ff80eabc9311b3055f7cdef884dee48
Max Pang <jpangruitongm@gmail.com>
2025-01-29 11:24:04 -0700

    Initial commit

---
+++ ChessBoardClassLibrary/Services/BusinessLogicLayer/BoardLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoardClassLibrary.Services.BusinessLogicLayer
+{
+    public class BoardLogic
+    {
+    }
+}
